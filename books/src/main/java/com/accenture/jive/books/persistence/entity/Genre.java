e816b9f8be45276102018cdbce98d0577889c627
Alina Gruenaug <alina.gruenaug@accenture.com>
2024-07-31 16:34:16 +0200

    added Genre

---
+++ books/src/main/java/com/accenture/jive/books/persistence/entity/Genre.java
@@ -0,0 +1,17 @@
+package com.accenture.jive.books.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
